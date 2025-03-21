34fdb98ef3e3f85b35194f2e4cef868d8e2ac073
Anton Kilk <anton.kilk@gamesglobal.com>
2025-03-21 11:08:50 +0200

    task 2-1 done

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -4,14 +4,40 @@ import { Task } from "./task.model";
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
+  private nextId: number = 1;
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task {
+    const task =  this.tasks.find((task) => task.id === id);
+    if (!task) throw new NotFoundException(`Task with ID "${id}" not found`);
+    return task;
+  }
 
-  createTask(task: Task): Task {}
+  createTask(task: Task): Task {
+    task.id = task.id || String(this.nextId++);
+    this.tasks.push(task);
+    return task;
+  }
 
-  updateTask(id: string, update: Task): Task {}
+  updateTask(id: string, update: Task): Task {
+    const task = this.getTaskById(id);
+    if (!task) throw new NotFoundException(`Task with ID "${id}" not found`);
+ 
+    task.title = update.title;
+    task.description = update.description;
+    task.status = update.status;
+    
+    return task;
+  }
 
-  deleteTask(id: string): Task {}
+  deleteTask(id: string): Task {
+    const task = this.getTaskById(id);
+    if (!task) throw new NotFoundException(`Task with ID "${id}" not found`);
+
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    return task;
+  }
 }
