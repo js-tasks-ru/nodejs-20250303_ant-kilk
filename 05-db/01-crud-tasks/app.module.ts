5a981042687d0b4833831da260f83e70296a2d99
Anton Kilk <akilk@student.hive.fi>
2025-03-31 11:25:27 +0300

    task 05-01 done

--- 05-db/01-crud-tasks/app.module.ts
+++ 05-db/01-crud-tasks/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Task } from "./tasks/entities/task.entity";
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "db.sqlite",
+      entities: [Task],
+      synchronize: true,
+      logging: false,
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
